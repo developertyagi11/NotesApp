5f169d2c9c2b850e8aa0e720baa6e90c158de9c8
chirag tyagi <tyagichirag.me@gmail.com>
2024-08-09 14:24:40 +0530

    small changes

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000")
 })
 
